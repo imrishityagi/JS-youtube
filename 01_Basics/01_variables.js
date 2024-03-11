3b67bf1a880a2a60bae6e24e0c8d5c252e6448d2
Rishabh Tyagi <officialrishabhtyagi@gmail.com>
2024-03-11 23:16:26 +0530

    added a directory from previous repo

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,48 @@
+// variables means which are changable or say whose value can be changed whenever we wish to change
+// constants means which are not changable once declared and whose value can not be changed if even we wish to change
+
+// how to declare variables in javascript:
+// there are two ways to declare variable in js
+// 1 - constant
+// 2 - variable   ----> 1 - var -- var was used in older days of js to declare variables but it has one problem which is of block scope and it is oudated way to declare variables in js
+//                ----> 2 - let -- this is the alternative of var keyword in js and is the ongoing way to declare variables in js
+
+const accountId = 799582; // this is contant variable and it's value can not be changed
+let accountEmail = "rishabh@gmail.com"; // this is variable
+var accountPassword = "12345"; // this is also variable
+accountCity = "Mumbai"; // this is possible in javascript but don't do this at all
+let accountDate; // this is also possible and it gives us undefined value in return bacause we din't provided a value to the varaible initially
+
+// accountId = 256682 // not allowed - trying to change the value of a constant variable
+
+accountEmail = "rt@rt.com";
+accountPassword = "21212121";
+accountCity = "Pune";
+
+console.log(accountId);
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountDate,
+]);
+
+// eg. of block scope problem with var defined variables
+if (true) {
+  var variableInBlock = "I am in a block!";
+}
+
+console.log(variableInBlock); // Output: "I am in a block!"
+// in this example, despite the var declaration being inside an if block, the variable variableInBlock is accessible outside the block. This is because var does not have block scope; it is function-scoped or globally scoped.
+
+
+// at the same time if we declare varible with let keyword then
+if (true) {
+    let blockScopedVariable = "I am block-scoped!";
+  }
+  
+  console.log(blockScopedVariable); // Error: blockScopedVariable is not defined
+//In this case, attempting to access blockScopedVariable outside the block results in an error because let is block-scoped, and the variable is not accessible outside the if block.
+
+// imp note -> prefer not to use var because of the problem of block scope and functional scope. 
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,53 @@
+"use strict" // this will treat all JS code as newer version of JS, and even if we don't write it than also the code will be treated as the newer version of js by default nowadays.
+
+// alert("Welcome") // we are using node Js, not the browser that's why we can't use alert this way. although there is a way to use alert in vs code or say in node, will discuss later 
+
+// console.log( 3 + 3 ); console.log("rishabh") // this is not a right way to write the JS code; please maintain readablility of the code 
+// if we remove the semi-colon from 1st console.log statement then it will give us error
+// so better to write it in a proper way like:
+// console.log( 3 + 3 )
+// console.log("rishabh")
+
+let name = "rishabh" // string
+let age = 22 // number
+let isLoggedIn = false // boolean
+
+// PRIMITIVE TYPES:
+// number => range = 2 to power 53 (approx-not sure)
+// bigint => used to store large numbers like in trading, but not used frequently
+// string => use always double "" (not mandatory we can even use '' also)
+// boolean => true/false
+// null => it a datatype as well as a standalone value or say type (null means absence of value)
+   // null is a representation of empty value but remember if we say is this a emplty value suppose this => let name = "" so here these empty quotes does't specify the empty value, its a string type not a null type
+   // example of null - we requested temperature from server and server returened 0 because of some issue then it is not a good idea to send 0 back because 0 is also a temperature. so in this case null will be sent back to show that there is some error that's why the null value is sent back
+// undefined => where value is not defined or say the value is not assigned till now
+// symbol => it is used to find uniqueness or say used to differeciate the components 
+
+
+// OBJECTS => will discuss in later videos
+// null is of object type
+
+// typeof in js:
+console.log(typeof age)
+console.log(typeof null)  // will return object - this is a historical quirk (strange) in js
+console.log(typeof undefined) // will return undefined as a type
+
+
+// notes:
+// in js we can use semicolon or may not but in most of the cases we try to avoid semicolon but at the same time it is a good practice to use semicolon to increase the readibility of the code
+// original documentation of js is available at - tc39.es
+// also we can use mdn documentaion because it is very clear and precise to read and understand
+
+// undefined - Declaration without Initialization: When a variable is declared without being assigned a value, it is automatically assigned the value undefined.
+
+// Function without Return Statement: If a function does not explicitly return a value, it returns undefined.
+
+// Function Returning Null: If a function returns the value null, the result of the function will be null.
+
+// Typeof Checks: The typeof operator can be used to check the type of a variable. Note that typeof null returns 'object', which can be misleading. This is a historical quirk in JavaScript.
+
+// very imp note:
+// Undefined: It means a variable has been declared but hasn't been assigned a value. It's the default value of uninitialized variables.
+
+// Null: It represents the intentional absence of any object value. It is a value that has been explicitly set to indicate no value or no object.
+// So, undefined typically shows up when JavaScript expects a value but hasn't found one, whereas null is often set by developers to explicitly indicate that a variable or object has no assigned value.
